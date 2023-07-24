924ab82cb646dbdfa0e2566f38cfcf4a40796877
Aaditya <82424609+Aaditya-Mishra1@users.noreply.github.com>
2023-07-24 11:14:26 +0530

    Porfolio

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Data Analyst","Web Designer","Gamer"],
+        strings: ["Data Analyst", "Website Developer", "Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Data Analyst", "Web Designer", "Gamer"],
+        strings: ["Data Analyst", "Website Developer", "Gamer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
@@ -70,5 +70,6 @@ $(document).ready(function(){
             }
         }
     });
-});
 
+    
+});
